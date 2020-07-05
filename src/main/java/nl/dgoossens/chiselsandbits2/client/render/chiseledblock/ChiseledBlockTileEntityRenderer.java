bc5c51bfcee5a2705e9c42ce38eecd4d825bdb27
Daniël Goossens <daniel@goossens.ch>
2020-07-05 23:01:58 +0200

    Update dummy chiseled block renderer to look somewhat close to the actual shape

--- src/main/java/nl/dgoossens/chiselsandbits2/client/render/chiseledblock/ChiseledBlockTileEntityRenderer.java
+++ src/main/java/nl/dgoossens/chiselsandbits2/client/render/chiseledblock/ChiseledBlockTileEntityRenderer.java
@@ -10,28 +10,30 @@ import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import nl.dgoossens.chiselsandbits2.common.blocks.ChiseledBlockTileEntity;
 
 public class ChiseledBlockTileEntityRenderer extends TileEntityRenderer<ChiseledBlockTileEntity> {
-    private final ModelRenderer singleBottom;
-
     public ChiseledBlockTileEntityRenderer(TileEntityRendererDispatcher rendererDispatcherIn) {
         super(rendererDispatcherIn);
-
-        this.singleBottom = new ModelRenderer(64, 64, 0, 19);
-        this.singleBottom.addBox(1.0F, 0.0F, 1.0F, 14.0F, 10.0F, 14.0F, 0.0F);
     }
 
     @Override
     public void render(ChiseledBlockTileEntity tileEntity, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLight, int combinedOverlay) {
-        // Rotate the matrix stack to face the proper direction
-        float f = Direction.NORTH.getHorizontalAngle();
-        matrixStack.translate(0.5D, 0.5D, 0.5D);
-        matrixStack.rotate(Vector3f.YP.rotationDegrees(-f));
-        matrixStack.translate(-0.5D, -0.5D, -0.5D);
+        //Rotate the matrix stack to face the proper direction
+        //float f = Direction.NORTH.getHorizontalAngle();
+        //matrixStack.translate(0.5D, 0.5D, 0.5D);
+        //matrixStack.rotate(Vector3f.YP.rotationDegrees(-f));
+        //matrixStack.translate(-0.5D, -0.5D, -0.5D);
 
-        // Get the vertex buffer to draw into and draw
+        //Get the vertex buffer to draw into and draw
         IVertexBuilder vertexBuilder = buffer.getBuffer(RenderType.getEntityCutout(new ResourceLocation("minecraft:missingno")));
-        this.singleBottom.render(matrixStack, vertexBuilder, combinedLight, combinedOverlay);
+        //TEMP Render a textureless rectangle within the selection bounding box
+        ModelRenderer mr = new ModelRenderer(64, 64, 0, 19);
+        VoxelShape selectionBox = tileEntity.getVoxelState().getSelectionShape();
+        mr.addBox((float) selectionBox.getStart(Direction.Axis.X) * 16, (float) selectionBox.getStart(Direction.Axis.Y) * 16, (float) selectionBox.getStart(Direction.Axis.Z) * 16,
+                (float) selectionBox.getEnd(Direction.Axis.X) * 16, (float) selectionBox.getEnd(Direction.Axis.Y) * 16, (float) selectionBox.getEnd(Direction.Axis.Z) * 16);
+        mr.render(matrixStack, vertexBuilder, combinedLight, combinedOverlay);
     }
 }
